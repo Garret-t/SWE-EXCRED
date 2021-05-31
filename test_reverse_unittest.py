ebed3267d1e2cef681eee541c01d6f63c978bba1
Garrett Scott <Garret-t@users.noreply.github.com>
2021-05-31 14:49:24 -0700

    created tests using unittest

---
+++ test_reverse_unittest.py
@@ -0,0 +1,18 @@
+import unittest
+from reverse import reverse_sent
+
+class TestReverse(unittest.TestCase):
+    def test_correct(self):
+        self.assertEqual(reverse_sent("My name is Garrett"), "Garrett is name My")
+
+    def test_single(self):
+        self.assertEqual(reverse_sent("Name"), "Name")
+    
+    def test_space(self):
+        self.assertEqual(reverse_sent(" "), " ")
+    
+    def test_empty(self):
+        self.assertEqual(reverse_sent(""), "")
+    
+if __name__ == "__main__":
+    unittest.main()
